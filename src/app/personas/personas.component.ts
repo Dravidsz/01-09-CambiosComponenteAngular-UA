981fd58d4a0badebba000bbf7e14a15322120710
Dravidss <hdavidortega32@gmai.com>
2024-09-02 12:22:23 -0500

    Inicio de proyecto de Angular

---
+++ src/app/personas/personas.component.ts
@@ -0,0 +1,14 @@
+//decorador
+import { Component } from "@angular/core";
+//para crear componentes
+@Component({
+    selector: 'app-personas',
+    templateUrl:'./personas.component.html'
+}
+)
+
+//definicion de clase y exportarla
+export class PersonasComponent{
+
+
+}
